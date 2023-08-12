20666bf67d9373d6e68c5ea42d7ecfde330dfce2
lucas4lves <zombnfl@gmail.com>
2023-08-12 16:32:46 -0300

    Sketching day 4

--- camp-cleanup/index.ts
+++ camp-cleanup/index.ts
@@ -1,2 +1,25 @@
 const input = require('node:fs').readFileSync('./input', 'utf8');
 
+const lines = input.split('\n')
+
+const formatPairs = (pair: Array<string>) =>{
+	let [a, b] = [pair[0], pair[1]];
+	return {
+		a: a.replace('-', ','),
+		b: b.replace('-', ',')
+	}
+}
+
+const pairs = lines.map((line : string) =>{
+	if(!line){
+		return; 
+	}
+
+	let pair = formatPairs(line.split(",")) 
+
+	return {
+		pair 
+	}
+})
+
+console.log(pairs);
